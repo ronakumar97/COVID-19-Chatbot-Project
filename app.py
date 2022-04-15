0e9c18b9c9384f2a579fae011571e0f25fec22c0
ronakumar97 <lifecoderonak2016@gmail.com>
2022-04-15 13:55:55 -0400

    Added code

---
+++ app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import spacy
+import json
+from chatbot_backend import preprocessing, tokenize_data, create_categorical_target, train_model
+import random
+import tensorflow as tf
+import numpy as np
+from streamlit_chat import message
+
+def get_response(input_text):
+    sent_seq = []
+    doc = nlp(repr(input_text))
+
+    for token in doc:
+        if token.text in tokenizer.word_index:
+            sent_seq.append(tokenizer.word_index[token.text])
+        else:
+            sent_seq.append(tokenizer.word_index['<unk>'])
+
+    sent_seq = tf.expand_dims(sent_seq, 0)
+    pred = model(sent_seq)
+
+    pred_class = np.argmax(pred.numpy(), axis=1)
+
+    return random.choice(intent_doc[trg_index_word[pred_class[0]]]), trg_index_word[pred_class[0]]
+
+@st.cache(allow_output_mutation=True)
+def train_chatbot(tokenizer, input_tensor, target_tensor):
+    return train_model(tokenizer, input_tensor, target_tensor)
+
+if __name__ == '__main__':
+    nlp = spacy.load('en_core_web_sm')
+
+    with open('intents.json') as f:
+        intents = json.load(f)
+
+    inputs, targets = [], []
+    classes = []
+    intent_doc = {}
+
+    for intent in intents['intents']:
+        if intent['intent'] not in classes:
+            classes.append(intent['intent'])
+        if intent['intent'] not in intent_doc:
+            intent_doc[intent['intent']] = []
+
+        for text in intent['text']:
+            inputs.append(preprocessing(text))
+            targets.append(intent['intent'])
+
+        for response in intent['responses']:
+            intent_doc[intent['intent']].append(response)
+
+    tokenizer, input_tensor = tokenize_data(inputs)
+    target_tensor, trg_index_word = create_categorical_target(targets)
+
+    model = train_chatbot(tokenizer, input_tensor, target_tensor)
+
+    if "history" not in st.session_state:
+        st.session_state.history = [
+            {"message": 'Welcome. I am your COVID-19 bot. How can I help ?', "is_user": False}
+        ]
+
+    st.title('COVID-19 Chatbot')
+
+    input_text = st.text_input(label="Ask me anything...")
+
+    if(input_text):
+        st.session_state.history.append({"message": input_text, "is_user": True})
+        result, type = get_response(input_text)
+
+        if(result):
+            st.session_state.history.append({"message": result, "is_user": False})
+
+    count = 0
+
+    for chat in st.session_state.history:
+        message(**chat, key=str(count))
+        count += 1
+
+
+
+
+
+
---
+++ chatbot_backend.py
@@ -0,0 +1,52 @@
+import re
+import tensorflow as tf
+
+def preprocessing(line):
+    line = re.sub(r'[^a-zA-z.?!\']', ' ', line)
+    line = re.sub(r'[ ]+', ' ', line)
+    return line
+
+def tokenize_data(input_list):
+    tokenizer = tf.keras.preprocessing.text.Tokenizer(filters='', oov_token='<unk>')
+    tokenizer.fit_on_texts(input_list)
+    input_seq = tokenizer.texts_to_sequences(input_list)
+    input_seq = tf.keras.preprocessing.sequence.pad_sequences(input_seq, padding='pre')
+    return tokenizer, input_seq
+
+def create_categorical_target(targets):
+    word = {}
+    categorical_target = []
+    counter = 0
+    for trg in targets:
+        if trg not in word:
+            word[trg] = counter
+            counter += 1
+        categorical_target.append(word[trg])
+
+    categorical_tensor = tf.keras.utils.to_categorical(categorical_target, num_classes=len(word), dtype='int32')
+    return categorical_tensor, dict((v, k) for k, v in word.items())
+
+def train_model(tokenizer, input_tensor, target_tensor):
+    epochs = 50
+    vocab_size = len(tokenizer.word_index) + 1
+    embed_dim = 512
+    units = 128
+    target_length = target_tensor.shape[1]
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Embedding(vocab_size, embed_dim),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units, dropout=0.2)),
+        tf.keras.layers.Dense(units, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(target_length, activation='softmax')
+    ])
+
+    optimizer = tf.keras.optimizers.Adam(lr=1e-2)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
+
+    early_stop = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=4)
+
+    model.fit(input_tensor, target_tensor, epochs=epochs, callbacks=[early_stop])
+
+    return model
